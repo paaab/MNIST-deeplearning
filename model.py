24ee6a7f846d2eb315574df3030e1748f1b590fa
Pablo B <p.bonilla.2024@alumnos.urjc.es>
2026-09-04 18:01:31 +0200

    add fc2

--- model.py
+++ model.py
@@ -19,6 +19,10 @@ class SimpleNN(nn.Module):
         #it is quite simple, it just converts negatives values from the outputs and turn them into 0 (ReLU(x) = max(0, x))
         #this ads non-linearity to our model
 
+        self.fc2 = nn.Linear(128, 10)
+        #same idea as fc1, but why 10? MNIST has 10 possible classes
+        #this 10 values are named logits, they are scores that show how much the model favors a class, not probabilities yet
+
     def forward(self, x):
         #This is the "path" that our data is going to follow, x is what we called the images that our model is going to receive
 
@@ -30,6 +34,8 @@ class SimpleNN(nn.Module):
         x = self.fc1(x) #data goes through fc1, 784 pixels -> 128 values
 
         x = self.relu(x) #we apply ReLU, here the shape doesnt change, 128 -> 128, but we "turn off" negative values
+
+        x = self.fc2(x) #applies fc2
 
         return x #returns final output
 
