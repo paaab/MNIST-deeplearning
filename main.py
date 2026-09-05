43af72b84ecb319bde9ba6bef462b725469da798
Pablo B <p.bonilla.2024@alumnos.urjc.es>
2026-09-05 16:09:51 +0200

    add loss function

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from torchvision import datasets
 from torchvision import transforms
 from torch.utils.data import DataLoader
+import torch
+import torch.nn as nn
+from model import SimpleNN
 
 transform = transforms.ToTensor() #neural networks work with tensors instead of images, transforms images into tensors,
                                   #dividing each pixel by 255 in order to get values between 0.0 and 1.0
@@ -23,6 +26,23 @@ train_loader = DataLoader(
 
     shuffle=True  #in order not to have every similar number in order (00000001111111222222), we shuffle the data
 )
+
+model = SimpleNN() #we create an object from our class SimpleNN
+
+criterion = nn.CrossEntropyLoss() #LOSS FUNCTION!!!
+#Our model receives an image and returns 10 logits, but we also have the label of each image
+#we choose CrossEntropy because we have 10 possible classes, it will evaluate how well our prediction matches the actual label
+#by that, we will receive a value of loss, the lower the loss, the less error our prediction has
+
+for images, labels in train_loader: #we obtain 32 images [32, 1, 28, 28] and 32 labels [0,7,4,8,1 ...]
+
+    outputs = model(images)
+    #here we call our model, which internally calls the method forward
+    #this is the path that it follows:
+    #images -> model(images) -> forward(images) -> flatten -> fc1 -> ReLU -> fc2 -> outputs
+
+    loss = criterion(outputs, labels)
+    #we compare the outputs from the model with the labels to know how much the model has failed
 
 
 
